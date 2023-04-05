0e3c182ea966871862923c8f109b4f6ff941bd46
ki11errabbit <ki11errabbit@posteo.net>
2023-04-05 15:54:00 -0600

    removed commented out code in spelling_corrector.hs

--- spelling_corrector.hs
+++ spelling_corrector.hs
@@ -18,38 +18,18 @@ data SpellingCorrector = SpellingCorrector {
 new_corrector :: SpellingCorrector
 new_corrector = SpellingCorrector (make_trie Nothing)
 
---getlines :: Handle -> IO [String]
---getlines h = Data.List.lines `fmap` hGetContents h
-
-{-makeLower :: [String] -> [String]
-makeLower [] = []
-makeLower input = Data.List.map Text.unpack (Data.List.map toLower (Data.List.map Text.pack input))-}
-
-{-file_to_list :: FilePath -> IO [String]
-file_to_list file_path = withFile file_path ReadMode $ \handle -> do
-  contents <- hGetContents handle
-  return $ map (map toLower) (lines contents) -}
-
 file_to_list :: FilePath -> IO ()
 file_to_list file_path = readFile file_path >>= print
 
-{-file_to_list :: FilePath -> IO [String]-}
 
 load_dictionary :: FilePath -> SpellingCorrector -> IO SpellingCorrector
 load_dictionary file_name corrector = do
   string <- readFile file_name
   return $ SpellingCorrector { dictionary = load_dictionary_helper (words string) (dictionary corrector)}
 
-{-load_dictionary :: String -> SpellingCorrector -> SpellingCorrector
-load_dictionary file_name corrector = do
-  list <- file_to_list file_name
-  SpellingCorrector { dictionary = load_dictionary_helper list (dictionary corrector)}-}
-  --corrector { dictionary = foldl (\dict word -> add_string word dict) (dictionary corrector) list}
                                      
 load_dictionary_helper :: [String] -> Trie -> Trie
 load_dictionary_helper file_name dict = foldl (\dict word -> add_string word dict) dict file_name
-{-load_dictionary_helper [] dict = dict
-load_dictionary_helper (x:xs) dict = load_dictionary_helper xs (add_string x dict)-}
 
 suggest_similar_word :: String -> SpellingCorrector -> Maybe String
 suggest_similar_word input corrector = do
