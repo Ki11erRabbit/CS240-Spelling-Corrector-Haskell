a0a60a267c4132def757b702dd301ac1eca3481b
ki11errabbit <ki11errabbit@posteo.net>
2023-04-05 14:41:18 -0600

    implemented all edit distance functions

--- spelling_corrector.hs
+++ spelling_corrector.hs
@@ -46,7 +46,7 @@ load_dictionary_helper :: [String] -> Trie -> Trie
 load_dictionary_helper [] dict = dict
 load_dictionary_helper (x:xs) dict = load_dictionary_helper xs (add_string x dict)
 
-suggest_similar_word :: String -> IO SpellingCorrector -> Maybe String
+suggest_similar_word :: String -> SpellingCorrector -> Maybe String
 suggest_similar_word input corrector = do
   let input_lower = map toLower input
   case find_string input_lower (dictionary corrector) of
@@ -54,7 +54,14 @@ suggest_similar_word input corrector = do
     Nothing ->  Nothing-- do
       --let edit_dist1 = gen_edit_dist1 input_lower
 
+testFunc :: String -> String -> IO ()
+testFunc file_name input = do
+  corrector <- load_dictionary file_name new_corrector
+  case suggest_similar_word input corrector of
+    Just word -> putStrLn word
+    Nothing -> putStrLn "No suggestions"
 
+  
 
 gen_edit_dist1 :: String -> HashSet.HashSet String
 gen_edit_dist1 word = HashSet.union (insert_char word) (HashSet.union (alternate_char word) (HashSet.union (delete_char word) (transpose_char word)))
@@ -67,13 +74,40 @@ delete_char_helper str pos = if length str == pos then [] else ((take pos str) +
 
 
 transpose_char :: String -> HashSet.HashSet String
-transpose_char str = HashSet.fromList (transpose_char_helper str 0 1)
+transpose_char str = HashSet.fromList (trans_helperI 0 str)
 
-transpose_char_helper :: String -> Int -> Int -> [String]
-transpose_char_helper str pos1 pos2 = if length str == pos1 then [] else "todo":transpose_char_helper str (pos1 +1) (pos2 +1)
 
+trans_helperI :: Int -> String -> [String]
+trans_helperI i str = if length str == i then [] else (trans_helperJ i (i +1) str) ++ trans_helperI (i +1) str
+
+trans_helperJ :: Int -> Int -> String -> [String]
+trans_helperJ i j str = if length str == j then [] else (replaceChar (str!!j) i (replaceChar (str!!i) j str)):trans_helperJ i (j +1) str
+  
+replaceChar :: Char -> Int -> String -> String
+replaceChar c pos str = (take pos str) ++ (c : (drop (pos+1) str))
+            
 alternate_char :: String -> HashSet.HashSet String
-alternate_char str = HashSet.fromList ["todo", "not","implemented"]
+alternate_char str = HashSet.fromList (alternate_char_helper str alphabet 0)
+  where alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+type Alphabet = String
+
+alternate_char_helper :: String -> Alphabet -> Int -> [String]
+alternate_char_helper str alphabet pos = if length str == pos then [] else (alt_char_helper pos 0 str alphabet) ++ alternate_char_helper str alphabet (pos + 1)
+
+alt_char_helper :: Int -> Int -> String -> Alphabet -> [String]
+alt_char_helper pos index str alphabet = if length alphabet == index then [] else (replaceChar (alphabet!!index) pos str):alt_char_helper pos (index + 1) str alphabet
 
 insert_char :: String -> HashSet.HashSet String
-insert_char str = HashSet.fromList ["todo", "not","implemented"]
+insert_char str = HashSet.fromList (insert_char_helper str alphabet 0)
+  where alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+insert_char_helper :: String -> Alphabet -> Int -> [String]
+insert_char_helper str alphabet pos = if length str == pos then insert_helper_end str alphabet else (insert_helper str alphabet pos 0) ++ insert_char_helper str alphabet (pos + 1)
+
+insert_helper :: String -> Alphabet -> Int -> Int -> [String]
+insert_helper str alphabet pos index = if length alphabet == index then [] else (take pos str ++ alphabet!!index : (drop pos str)):insert_helper str alphabet pos (index + 1)
+
+insert_helper_end :: String -> Alphabet -> [String]
+insert_helper_end str [] = []
+insert_helper_end str (x:xs) = (str ++ [x]):insert_helper_end str xs
