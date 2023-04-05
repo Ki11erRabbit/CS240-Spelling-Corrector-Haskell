6a6107ae3afe0840a1ae37024ef8a7ad6b43de33
ki11errabbit <ki11errabbit@posteo.net>
2023-04-05 16:45:11 -0600

    made spelling corrector have some different modes

--- main.hs
+++ main.hs
@@ -1,18 +1,44 @@
 import SpellingCorrector
 
-
+import System.Environment
+import Data.List
 
 main :: IO ()
 main = do
+  args <- getArgs
+  case length args of
+    0 -> interactiveMode
+    1 -> if head args == "--help" then
+      putStrLn "Usage: spelling-corrector [dictionary file] [word to check]"
+      else do
+      let dictFile = head args
+      putStrLn "Reading dictionary..."
+      let spcorrector = new_corrector
+      corrector <- load_dictionary dictFile spcorrector
+      putStrLn "Dictionary loaded"
+      mainLoop corrector
+    2 -> do
+      let dictFile = head args
+      let word = args !! 1
+      putStrLn "Reading dictionary..."
+      let spcorrector = new_corrector
+      corrector <- load_dictionary dictFile spcorrector
+      putStrLn "Dictionary loaded"
+      let suggestion = suggest_similar_word word corrector
+      case suggestion of 
+        Nothing -> putStrLn "No suggestions found"
+        Just suggestion -> putStrLn $ "Suggestion: " ++ suggestion
+
+
+interactiveMode :: IO ()
+interactiveMode = do
   putStrLn "Enter a dictionary file: "
   dictFile <- getLine
-  putStrLn "Reading dictionary...\nThis may take a while..."
+  putStrLn "Reading dictionary..."
   let spcorrector = new_corrector
   corrector <- load_dictionary dictFile spcorrector
   putStrLn "Dictionary loaded"
   mainLoop corrector
-
-
 
 mainLoop :: SpellingCorrector -> IO ()
 mainLoop corrector = do
@@ -26,3 +52,5 @@ mainLoop corrector = do
         Nothing -> putStrLn "No suggestions found"
         Just suggestion -> putStrLn $ "Suggestion: " ++ suggestion
       mainLoop corrector
+
+
