5cd246eb7479337bc049fe7418ab68bba881575c
ki11errabbit <ki11errabbit@posteo.net>
2023-04-06 10:57:02 -0600

    made the repl part have more options but changing the dictionary still needs more work

--- Main.hs
+++ Main.hs
@@ -2,6 +2,8 @@ import SpellingCorrector
 
 import System.Environment
 import Data.List
+import Trie (add_string)
+
 
 main :: IO ()
 main = do
@@ -44,9 +46,21 @@ mainLoop :: SpellingCorrector -> IO ()
 mainLoop corrector = do
   putStrLn "Enter a word to check: (:q to quit)"
   word <- getLine
-  if word == ":q"
-    then return ()
-    else do
+  case word of
+    ":q" -> return ()
+    (':':'l':' ':word) -> do
+      let n_corrector' = SpellingCorrector (add_string word (dictionary corrector))
+      mainLoop n_corrector'
+    ":o" -> do
+      putStrLn "Options:\n :q - quit\n :l [word] - add word to dictionary\n :o - show options\n :r - reload dictionary\n :s - show dictionary"
+      mainLoop corrector
+    (':':'r':' ':dict_name) -> do
+      n_corrector <- load_dictionary dict_name corrector
+      mainLoop n_corrector
+    ":s" -> do
+      putStrLn $ "Dictionary: " ++ (show $ dictionary corrector)
+      mainLoop corrector
+    _ -> do
       let suggestion = suggest_similar_word word corrector
       case suggestion of 
         Nothing -> putStrLn "No suggestions found"
